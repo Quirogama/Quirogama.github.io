e454a54edbc525098ea6fd58a120fca9b2098861
Andrés Quiroga <quiroga3719@gmail.com>
2025-10-18 20:56:36 -0500

    Windows 2.0 prueba

---
+++ .svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.DLL0Xi3R.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/CS-ZYib0.js","_app/immutable/chunks/Gh2S5eGj.js","_app/immutable/chunks/cxH3LUjo.js"];
+export const stylesheets = [];
+export const fonts = [];
---
+++ .svelte-kit/output/server/nodes/2.js
@@ -0,0 +1,8 @@
+
+
+export const index = 2;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/2._-C58uOM.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/CS-ZYib0.js","_app/immutable/chunks/Gh2S5eGj.js","_app/immutable/chunks/D5_2jSvg.js","_app/immutable/chunks/CpW7Jdrt.js"];
+export const stylesheets = ["_app/immutable/assets/Taskbar.9pw2niHH.css"];
+export const fonts = [];
