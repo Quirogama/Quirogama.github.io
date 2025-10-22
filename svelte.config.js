766535f4126237204b4d46ad0b8587574c96573a
Andrés Quiroga <quiroga3719@gmail.com>
2025-10-22 00:44:19 -0500

    Index en docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+  kit: {
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+      fallback: 'index.html' // si quieres comportamiento SPA para rutas no prerenderizadas
+    })
+    // NO pongas `prerender: { default: true }` aquí si te da error
+  }
+};
 
 export default config;
