0b611a4089e5a8196771c0e94baf2b9741605130
Andrés Quiroga <quiroga3719@gmail.com>
2025-10-21 23:27:19 -0500

    Version sin index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Desktop from '$lib/Desktop.svelte';
+	import Taskbar from '$lib/Taskbar.svelte';
+
+	let windows = [
+		{
+			id: 1,
+			title: 'Welcome',
+			width: 360,
+			height: 220,
+			z: 2,
+			content: 'Hola! Bienvenido al Windows 98 de prueba.'
+		}
+	];
+
+	let tasks = ['Welcome'];
+</script>
+
+<Desktop {windows} />
+<Taskbar {tasks} />
